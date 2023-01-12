01f82b1b247e6b72ca8292d63dc083f2d84b4581
282604691 <93993202+282604691@users.noreply.github.com>
2023-01-13 05:25:06 +0800

    system

---
+++ quiz/src/main/java/edu/ccsfu/quiz/domain/Paper.java
@@ -0,0 +1,14 @@
+package edu.ccsfu.quiz.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Paper {
+    private int paperId;
+    private String paperName;
+    private int scoreSin;
+    private int scoreChe;
+    private int scoreJug;
+}
---
+++ quiz/src/main/java/edu/ccsfu/quiz/mapper/ClasseMapper.java
@@ -0,0 +1,27 @@
+package edu.ccsfu.quiz.mapper;
+
+import edu.ccsfu.quiz.domain.Classe;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface ClasseMapper {
+    List<Classe> queryAll();
+
+    void addClasse(Classe classe);
+
+    Classe queryClasseById(Integer id);
+
+    void editClasse(Classe classe);
+
+    void deleteClasseById(Integer id);
+
+    Classe queryByTeacherId(Integer id);
+
+    List<Classe> queryAllTeacherId();
+
+    Classe queryClaNameById(int claId);
+}
